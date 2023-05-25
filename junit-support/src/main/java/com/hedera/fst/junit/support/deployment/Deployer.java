76b0d76f460739239e93a1c9e2c8c906ecec6d4f
Jeromy Cannon <jeromy@swirldslabs.com>
2023-05-25 15:22:50 -0500

    deploy/delete tests run together
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- junit-support/src/main/java/com/hedera/fst/junit/support/deployment/Deployer.java
+++ junit-support/src/main/java/com/hedera/fst/junit/support/deployment/Deployer.java
@@ -7,11 +7,13 @@ import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.Watch;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Deployer {
     int runningCount = 0;
+
 
     public void deployFromResource(String resourceName, String namespace) throws InterruptedException {
         try (KubernetesClient client = new KubernetesClientBuilder().build()) {
@@ -19,23 +21,25 @@ public class Deployer {
                     .inNamespace(namespace)
                     .createOrReplace();
 
+            Thread.sleep(3000);
             PodList podList = client.pods().inNamespace(namespace).list();
-            List<Watch> watchList = new ArrayList<>();
             for (Pod pod : podList.getItems()) {
-                watchList.add(client.pods().inNamespace(namespace).withName(pod.getMetadata().getName()).watch(new Watcher<>() {
-                    @Override
-                    public void eventReceived(Action action, Pod resource) {
-                        System.out.println("Event received: " + action.name() + " " + resource.getMetadata().getName());
-                        if (resource.getStatus().getPhase().equals("Running")){
-                            runningCount++;
-                        }
-                    }
+                client.pods().inNamespace(namespace).withName(pod.getMetadata().getName())
+                        .watch(new Watcher<>() {
+                            @Override
+                            public void eventReceived(Action action, Pod resource) {
+                                System.out.println(LocalDateTime.now() +
+                                        " Event received: " + action.name() + " " + resource.getMetadata().getName());
+                                if (resource.getStatus().getPhase().equals("Running")) {
+                                    runningCount++;
+                                }
+                            }
 
-                    @Override
-                    public void onClose(WatcherException cause) {
-                        System.out.println("Watcher close due to " + cause);
-                    }
-                }));
+                            @Override
+                            public void onClose(WatcherException cause) {
+                                System.out.println("Watcher close due to " + cause);
+                            }
+                        });
             }
             while (runningCount < podList.getItems().size()) {
                 Thread.sleep(1000);
@@ -43,11 +47,48 @@ public class Deployer {
         }
     }
 
-    public void deleteFromResource(String resourceName, String namespace) {
+    public void deleteFromResource(String resourceName, String namespace) throws InterruptedException {
         try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+            PodList podList = client.pods().inNamespace(namespace).list();
+            runningCount = podList.getItems().size();
             client.load(Deployer.class.getResourceAsStream(resourceName))
                     .inNamespace(namespace)
                     .delete();
+            for (Pod pod : podList.getItems()) {
+                client.pods().inNamespace(namespace).withName(pod.getMetadata().getName())
+                        .watch(new Watcher<>() {
+                            @Override
+                            public void eventReceived(Action action, Pod resource) {
+                                System.out.println(LocalDateTime.now() +
+                                        " Event received: " + action.name() + " " + resource.getMetadata().getName()
+                                        + " "
+                                        + resource.getStatus().getPhase());
+                                if (action.name().equals("DELETED")) {
+                                    runningCount--;
+//                                    PodList podList = client.pods().inNamespace(namespace).list();
+//                                    System.out.println(
+//                                            LocalDateTime.now() + " Pods remaining: " + podList.getItems().size());
+//                                    try {
+//                                        Thread.sleep(1000);
+//                                    } catch (InterruptedException e) {
+//                                        throw new RuntimeException(e);
+//                                    }
+//                                    podList = client.pods().inNamespace(namespace).list();
+//                                    System.out.println(
+//                                            LocalDateTime.now() + " Pods remaining: " + podList.getItems().size());
+//                                }
+                                }
+                            }
+
+                            @Override
+                            public void onClose(WatcherException cause) {
+                                System.out.println("Watcher close due to " + cause);
+                            }
+                        });
+            }
+            while (runningCount > 0) {
+                Thread.sleep(1000);
+            }
         }
     }
 }
--- junit-support/src/test/java/com/hedera/fst/junit/support/deployment/DeployerTest.java
+++ junit-support/src/test/java/com/hedera/fst/junit/support/deployment/DeployerTest.java
@@ -1,9 +1,14 @@
 package com.hedera.fst.junit.support.deployment;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(OrderAnnotation.class)
 class DeployerTest {
     @Test
+    @Order(1)
     void testDeployFromResource() throws InterruptedException {
         Deployer deployer = new Deployer();
         deployer.deployFromResource("/fst-deployment.yaml", "fst-ns");
@@ -11,7 +16,8 @@ class DeployerTest {
     }
 
     @Test
-    void testDeleteFromResource() {
+    @Order(2)
+    void testDeleteFromResource() throws InterruptedException {
         Deployer deployer = new Deployer();
         deployer.deleteFromResource("/fst-deployment.yaml", "fst-ns");
     }
