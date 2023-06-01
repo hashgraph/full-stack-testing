589c67d6e96952b0f0adc11840285c97b58c745a
Jeromy Cannon <jeromy@swirldslabs.com>
2023-06-01 15:47:11 -0500

    ./gradlew :spotlessApply
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,12 +22,19 @@ includeBuild(".") // https://github.com/gradlex-org/java-module-dependencies/iss
 
 // Include the subprojects
 include(":fullstack-bom")
+
 include(":fullstack-base-api")
+
 include(":fullstack-helm-client")
+
 include(":fullstack-junit-support")
+
 include(":fullstack-reporting")
+
 include(":fullstack-reporting-api")
+
 include(":fullstack-validator")
+
 include(":fullstack-validator-api")
 
 gradleEnterprise {
