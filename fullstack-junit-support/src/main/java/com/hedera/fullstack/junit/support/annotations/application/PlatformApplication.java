2e13be242cd63728aa2db2084f753321e6efcb7d
Nathan Klick <nathan@swirldslabs.com>
2023-08-11 16:17:27 -0500

    chore: update javadoc
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- fullstack-junit-support/src/main/java/com/hedera/fullstack/junit/support/annotations/application/PlatformApplication.java
+++ fullstack-junit-support/src/main/java/com/hedera/fullstack/junit/support/annotations/application/PlatformApplication.java
@@ -18,12 +18,40 @@ package com.hedera.fullstack.junit.support.annotations.application;
 
 import java.lang.annotation.*;
 
+/**
+ * Configures the Platform application to be executed by a test. This annotation may be applied to a test class or
+ * test method. If applied to a test class, the configuration will be applied to all test methods in the class. If
+ * applied to a test method, the configuration will be applied to only that test method. If applied to both a test
+ * class and a test method, the configuration applied to the test method will override the configuration applied to
+ * the test class.
+ */
 @Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface PlatformApplication {
+    /**
+     * Specifies the name of the Platform application jar file. The default value is {@code HederaNode.jar}. Only the
+     * name of the JAR file should be specified, not the path. The JAR file must be located in the {@code data/apps}
+     * directory of the Platform deployment.
+     *
+     * <p>
+     * This value is used to form the {@code <jar-name>} portion of {@code app, <jar-name>, <parameters>} specification
+     * for the Platform SDK {@code config.txt} definition.
+     *
+     * @return the name of the Platform application jar file.
+     */
     String fileName() default "HederaNode.jar";
 
+    /**
+     * Specifies the parameters to be passed to the Platform application. The default value is an empty array. The
+     * parameters are passed to the Platform application in the order specified. The values must not contain any commas.
+     *
+     * <p>
+     * This value is used to form the {@code <parameters>} portion of {@code app, <jar-name>, <parameters>} specification
+     * for the Platform SDK {@code config.txt} definition.
+     *
+     * @return the parameters to be passed to the Platform application.
+     */
     String[] parameters() default {};
 }
--- fullstack-junit-support/src/main/java/com/hedera/fullstack/junit/support/annotations/application/PlatformConfiguration.java
+++ fullstack-junit-support/src/main/java/com/hedera/fullstack/junit/support/annotations/application/PlatformConfiguration.java
@@ -17,15 +17,26 @@
 package com.hedera.fullstack.junit.support.annotations.application;
 
 import com.hedera.fullstack.junit.support.annotations.core.ConfigurationValue;
+
 import java.lang.annotation.*;
 
 /**
- *
+ * Specifies the configuration values used by the Platform SDK {@code settings.txt} file. This annotation may be applied
+ * to a test class or to a test method. If applied to a test class, the configuration values are applied to all test
+ * methods in the class. If applied to a test method, the configuration values are applied only to that test method. If
+ * applied to both a test class and a test method, the configuration values will be merged with the overlapping values
+ * specified by the test method taking precedence. These values are always merged with the default values with
+ * the overlapping values supplied by this annotation taking precedence.
  */
 @Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface PlatformConfiguration {
+    /**
+     * The configuration values to be applied to the Platform SDK {@code settings.txt} file.
+     *
+     * @return an array of configuration values.
+     */
     ConfigurationValue[] value();
 }
