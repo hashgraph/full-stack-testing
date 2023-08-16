28df3141ec42379ac396439ea3f961cf114d4bc2
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-08-16 15:01:54 +1000

    feat: add setup files for haproxy ingress controller
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- dev/scripts/gateway.sh
+++ dev/scripts/gateway.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+readonly SCRIPT_DIR
+
 GATEWAY_API_VERSION="${GATEWAY_API_VERSION:-v0.7.1}"
+
+function deploy_haproxy_ingress_controller() {
+  helm repo add haproxy-ingress https://haproxy-ingress.github.io/charts
+  helm install haproxy-ingress haproxy-ingress/haproxy-ingress\
+    --create-namespace --namespace ingress-controller\
+    --version 0.14.4\
+    -f "${SCRIPT_DIR}/../../charts/hedera-network/templates/gateway-api/haproxy-ingress-values.yaml"
+}
 
 function deploy_gateway_api_crd() {
   kubectl kustomize\
