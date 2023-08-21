5b0aabe4d18d4a8050272d96042a03d5563c8b80
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-08-21 11:28:02 +1000

    fix: working http route using envoy-gateway
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- dev/scripts/gateway.sh
+++ dev/scripts/gateway.sh
@@ -1,48 +1,82 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-readonly SCRIPT_DIR
-
-GATEWAY_API_VERSION="${GATEWAY_API_VERSION:-v0.7.1}"
-
-function deploy_haproxy_ingress_controller() {
+function deploy_envoy_gateway_api() {
   echo ""
-	echo "Deploying HAProxy Ingress Controller"
+	echo "Installing Envoy Gateway API"
   echo "-----------------------------------------------------------------------------------------------------"
-  local count=$(kubectl --namespace ingress-controller get services haproxy-ingress | grep 'haproxy-ingress' | wc -l)
-
-  if [[ $count -eq 0 ]]; then
-    helm repo add haproxy-ingress https://haproxy-ingress.github.io/charts
-    helm install haproxy-ingress haproxy-ingress/haproxy-ingress\
-      --create-namespace --namespace ingress-controller\
-      --version 0.14.4
+  local helm_chart=$(helm list --all-namespaces | grep envoy-gateway)
+  if [[ ! "${helm_chart}" ]]; then
+    helm install envoy-gateway oci://docker.io/envoyproxy/gateway-helm --version v0.0.0-latest -n envoy-gateway-system --create-namespace
+    kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
   else
-	  echo "HAProxy Ingress Controller is already installed"
-	  echo ""
+    echo "Envoy Gateway API is already installed"
+    echo ""
   fi
 }
 
-function deploy_gateway_api_crd() {
+function get_gateway_status() {
   echo ""
-	echo "Deploying Gateway API CRD"
+  helm list --all-namespaces | grep envoy-gateway
   echo "-----------------------------------------------------------------------------------------------------"
-  #  Expected list of CRDs
-  #  -------------------------
-  #  gatewayclasses.gateway.networking.k8s.io
-  #  gateways.gateway.networking.k8s.io
-  #  httproutes.gateway.networking.k8s.io
-  #  referencepolicies.gateway.networking.k8s.io
-  #  tcproutes.gateway.networking.k8s.io
-  #  tlsroutes.gateway.networking.k8s.io
-  #  udproutes.gateway.networking.k8s.io
-  local crd_count=$(kubectl get crd | grep "gateway.networking.k8s.io" | wc -l)
+  kubectl get crd
+  echo "-----------------------------------------------------------------------------------------------------"
+  kubectl get gatewayclass
+  echo "-----------------------------------------------------------------------------------------------------"
+  kubectl get gateway
+  echo "-----------------------------------------------------------------------------------------------------"
+  kubectl get httproute
+  echo "-----------------------------------------------------------------------------------------------------"
+  kubectl get grpcroute
+  echo "-----------------------------------------------------------------------------------------------------"
+}
 
-  if [[ $crd_count -ne 7 ]]; then
-    kubectl kustomize\
-     "github.com/kubernetes-sigs/gateway-api/config/crd?ref=${GATEWAY_API_VERSION}" |\
-     kubectl apply -f -
-	else
-	  echo "Gateway API CRD is already installed"
-	  echo ""
+function destroy_envoy_gateway_api() {
+  echo ""
+	echo "Uninstalling Envoy Gateway API"
+  echo "-----------------------------------------------------------------------------------------------------"
+  get_gateway_status
+
+  # Uninstall helm chart
+  local helm_chart=$(helm list --all-namespaces | grep envoy-gateway)
+  if [[ "${helm_chart}" ]]; then
+    helm uninstall envoy-gateway -n envoy-gateway-system
+    kubectl delete ns envoy-gateway-system
+  fi
+
+#  uninstall_crd "gateway.networking.k8s.io"
+#  uninstall_crd "gateway.envoyproxy.io"
+
+	echo "Envoy Gateway API is uninstalled"
+	echo ""
+}
+
+function uninstall_crd() {
+  local pattern="${1}"
+  local crds=$(kubectl get crd | grep "${pattern}" | awk '{print $1 }')
+
+  if [[ "${crds}" != "" ]]; then
+    read -a installed -d '' -r <<< "${crds}" || true
+    for name in "${installed[@]}"; do
+      echo "Uninstalling CRD: $name"
+      kubectl delete crd "${name}"
+    done
 	fi
+}
+
+function expose_envoy_gateway_svc() {
+  export ENVOY_SERVICE=$(kubectl get svc -n envoy-gateway-system --selector=gateway.envoyproxy.io/owning-gateway-namespace=default,gateway.envoyproxy.io/owning-gateway-name=fst -o jsonpath='{.items[0].metadata.name}')
+  echo ""
+	echo "Exposing Envoy Gateway Service: ${ENVOY_SERVICE} on port 8888"
+  echo "-----------------------------------------------------------------------------------------------------"
+  kubectl port-forward "svc/${ENVOY_SERVICE}"  -n envoy-gateway-system 8888:80 &
+}
+
+function unexpose_envoy_gateway_svc() {
+  pid=$(ps aux | grep "kubectl port-forward svc/envoy-default-fst-a00b59fc -n envoy-gateway-system 8888:80" | sed -n 2p | awk '{ print $2 }')
+  if [[ "${pid}" ]]; then
+    echo ""
+    echo "Un-exposing Envoy Gateway Service: ${ENVOY_SERVICE} on port 8888 for PID: ${pid}"
+    echo "-----------------------------------------------------------------------------------------------------"
+    kill -9 "${pid}"
+  fi
 }
