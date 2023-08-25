cb1e719ddd57c791ef8ae2e71b002a8217bec2bb
Jeromy Cannon <jeromy@swirldslabs.com>
2023-08-25 19:28:55 +0100

    added helm client test module
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

---
+++ fullstack-helm-client/src/test/java/module-info.java
@@ -0,0 +1,25 @@
+module com.hedera.fullstack.helm.client.test {
+    opens com.hedera.fullstack.helm.client.test to
+            org.junit.platform.commons;
+    opens com.hedera.fullstack.helm.client.test.execution to
+            org.junit.platform.commons;
+    opens com.hedera.fullstack.helm.client.test.model to
+            org.junit.platform.commons;
+    opens com.hedera.fullstack.helm.client.test.model.chart to
+            org.junit.platform.commons;
+    opens com.hedera.fullstack.helm.client.test.proxy.request.chart to
+            org.junit.platform.commons;
+    opens com.hedera.fullstack.helm.client.test.software to
+            org.junit.platform.commons;
+
+    requires com.hedera.fullstack.base.api;
+    requires org.assertj.core;
+    requires org.junit.jupiter.api;
+    requires com.hedera.fullstack.helm.client;
+    requires com.jcovalent.junit.logging;
+    requires org.junit.jupiter.params;
+    requires org.slf4j;
+    requires org.mockito;
+    requires org.mockito.junit.jupiter;
+    requires com.fasterxml.jackson.databind;
+}
