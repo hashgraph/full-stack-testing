d5aeab3fbc89932d3e9361b85c489d1eb94f5636
Nathan Klick <nathan@swirldslabs.com>
2023-09-07 13:20:50 -0500

    chore: introduces the build-logic project instead of buildSrc
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

---
+++ build-logic/project-plugins/build.gradle.kts
@@ -0,0 +1,21 @@
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:1.20.0")
+    implementation("org.gradlex:extra-java-module-info:1.4")
+    implementation("org.gradlex:java-ecosystem-capabilities:1.1")
+    implementation("org.gradlex:java-module-dependencies:1.3")
+
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.18.0")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.0.0.2929")
+    implementation("com.adarshr:gradle-test-logger-plugin:3.2.0")
+    implementation("net.swiftzer.semver:semver:1.1.2")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,7 @@
  */
 
 plugins {
-    id("com.autonomousapps.dependency-analysis")
-    id("com.hedera.fullstack.aggregate-reports")
-    id("com.hedera.fullstack.spotless-conventions")
-    id("com.hedera.fullstack.spotless-kotlin-conventions")
+    id("com.hedera.fullstack.umbrella")
 }
 
 repositories {
