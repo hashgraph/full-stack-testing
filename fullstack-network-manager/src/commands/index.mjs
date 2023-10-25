93a0724c18eeb01e6aaefffa05b1c8e3814874dc
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-10-25 11:02:07 +1100

    feat: scaffold network deploy command
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- fullstack-network-manager/src/commands/index.mjs
+++ fullstack-network-manager/src/commands/index.mjs
@@ -1,5 +1,6 @@
 import {ClusterCommand} from "./cluster.mjs";
 import {InitCommand} from "./init.mjs";
+import {NetworkCommand} from "./network.mjs"
 
 /*
  * Return a list of Yargs command builder to be exposed through CLI
@@ -8,10 +9,12 @@ import {InitCommand} from "./init.mjs";
 function Initialize(opts) {
     const initCmd = new InitCommand(opts)
     const clusterCmd = new ClusterCommand(opts)
+    const networkCmd = new NetworkCommand(opts)
 
     return [
         InitCommand.getCommandDefinition(initCmd),
         ClusterCommand.getCommandDefinition(clusterCmd),
+        NetworkCommand.getCommandDefinition(networkCmd),
     ]
 }
 
