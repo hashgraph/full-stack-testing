e64af3eb0456e97a1362cbc03f48a85a32753909
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-11-06 09:54:05 +1100

    feat: implement package downloader module
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

---
+++ fullstack-network-manager/test/e2e/core/package_downloader_e2e.test.js
@@ -0,0 +1,26 @@
+import {describe, expect, it} from "@jest/globals";
+import * as core from "../../../src/core/index.mjs";
+import {PackageDownloader} from "../../../src/core/package_downloader.mjs";
+import * as fs from 'fs'
+import * as path from "path";
+import * as os from "os";
+
+describe('DownloaderE2E', () => {
+    const testLogger = core.logging.NewLogger('debug')
+    const downloader = new PackageDownloader(testLogger)
+
+        it('should succeed with a valid tag', async () => {
+            let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'downloader-'));
+
+            let tag = 'v0.42.5'
+            let destPath = `${tmpDir}/build-${tag}.zip`
+            await expect(downloader.fetchPlatform(tag, tmpDir)).resolves.toBe(destPath)
+            expect(fs.existsSync(destPath)).toBeTruthy()
+            testLogger.showUser(destPath)
+
+            // remove the downloaded files to reduce disk usage
+            fs.rmSync(`${tmpDir}/build-${tag}.zip`)
+            fs.rmSync(`${tmpDir}/build-${tag}.sha384`)
+            fs.rmdirSync(tmpDir)
+        }, 100000)
+})
