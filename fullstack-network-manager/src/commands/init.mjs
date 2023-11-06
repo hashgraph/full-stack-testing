dd2580280075de8bcba43b5fbc90d820c38f6f24
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-11-06 12:42:25 +1100

    feat: setup home directory on init
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- fullstack-network-manager/src/commands/init.mjs
+++ fullstack-network-manager/src/commands/init.mjs
@@ -1,37 +1,74 @@
 import {BaseCommand} from "./base.mjs";
 import * as core from "../core/index.mjs"
 import chalk from "chalk";
+import {constants} from "../core/index.mjs";
+import * as fs from 'fs'
+import {FullstackTestingError} from "../core/errors.mjs";
 
 /**
  * Defines the core functionalities of 'init' command
  */
 export class InitCommand extends BaseCommand {
     /**
+     * Setup home directories
+     * @param dirs a list of directories that need to be created in sequence
+     * @returns {Promise<void>}
+     */
+    async setupHomeDirectory(dirs = [
+        constants.FST_HOME_DIR,
+        constants.FST_LOGS_DIR,
+        constants.FST_HEDERA_RELEASES_DIR,
+    ]) {
+        const self = this
+
+        try {
+            dirs.forEach(dirPath => {
+                if (!fs.existsSync(dirPath)) {
+                    fs.mkdirSync(dirPath)
+                }
+                self.logger.showUser(chalk.green(`OK: setup directory: ${dirPath}`))
+            })
+        } catch (e) {
+            this.logger.error(e)
+            throw new FullstackTestingError(e.message, e)
+        }
+    }
+
+    /**
      * Executes the init CLI command
      * @returns {Promise<boolean>}
      */
     async init() {
-        const deps = [
-            core.constants.HELM,
-            core.constants.KIND,
-            core.constants.KUBECTL,
-        ]
+        try {
+            await this.setupHomeDirectory()
 
-        const status = await this.checkDependencies(deps)
-        if (!status) {
+            const deps = [
+                core.constants.HELM,
+                core.constants.KIND,
+                core.constants.KUBECTL,
+            ]
+
+            const status = await this.checkDependencies(deps)
+            if (!status) {
+                return false
+            }
+
+            this.logger.showUser(chalk.green("OK: All required dependencies are found: %s"), chalk.yellow(deps))
+
+
+
+            return status
+        } catch (e) {
+            this.logger.showUserError(e)
             return false
         }
-
-        this.logger.showUser(chalk.green("OK: All required dependencies are found: %s"), chalk.yellow(deps))
-
-        return status
     }
 
     /**
      * Return Yargs command definition for 'init' command
      * @param initCmd an instance of InitCommand
      */
-    static getCommandDefinition(initCmd){
+    static getCommandDefinition(initCmd) {
         return {
             command: "init",
             desc: "Perform dependency checks and initialize local environment",
--- fullstack-network-manager/src/core/constants.mjs
+++ fullstack-network-manager/src/core/constants.mjs
@@ -4,6 +4,8 @@ import {fileURLToPath} from "url"
 // directory of this fle
 const CUR_FILE_DIR = dirname(fileURLToPath(import.meta.url))
 const USER = `${process.env.USER}`
+const FST_HOME_DIR = `${process.env.HOME}/.fsnetman`
+
 export const constants = {
     USER: `${USER}`,
     CLUSTER_NAME: `fst`,
@@ -13,6 +15,8 @@ export const constants = {
     KIND: 'kind',
     KUBECTL: 'kubectl',
     CWD: process.cwd(),
-    FST_HOME_DIR: process.env.HOME + "/.fsnetman",
+    FST_HOME_DIR: FST_HOME_DIR,
+    FST_LOGS_DIR: `${FST_HOME_DIR}/logs`,
+    FST_HEDERA_RELEASES_DIR: `${FST_HOME_DIR}/hedera-releases`,
     RESOURCES_DIR: normalize(CUR_FILE_DIR + "/../../resources")
 }
