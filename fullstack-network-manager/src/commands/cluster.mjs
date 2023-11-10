803ee4b4616fad751c45fc8ab961e274e4bb920d
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-11-11 07:35:43 +1100

    fix: set namespace to kubectl context after cluster create (#521)
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- fullstack-network-manager/src/commands/cluster.mjs
+++ fullstack-network-manager/src/commands/cluster.mjs
@@ -89,7 +89,7 @@ export class ClusterCommand extends BaseCommand {
                 this.logger.showUser(chalk.green('OK'), `namespace '${namespace}' already exists`)
             }
 
-            // TODO: kubectl config set-context --current --namespace="${NAMESPACE}"
+            await this.kubectl.config(`set-context --current --namespace="${namespace}"`)
 
             this.showList("namespaces", await this.getNameSpaces())
 
