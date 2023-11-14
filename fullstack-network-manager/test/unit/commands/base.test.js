4c72a77651b3409c2d4658757a8b515017d1a410
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-11-14 13:30:45 +1100

    fix: apply eslint
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- fullstack-network-manager/test/unit/commands/base.test.js
+++ fullstack-network-manager/test/unit/commands/base.test.js
@@ -1,56 +1,56 @@
-import {test, expect, it, describe} from "@jest/globals";
-import {ChartManager, ConfigManager, Helm, Kubectl, logging} from "../../../src/core/index.mjs";
-import {BaseCommand} from "../../../src/commands/base.mjs";
-import * as core from "../../../src/core/index.mjs"
-import {Kind} from "../../../src/core/kind.mjs";
+import { expect, it, describe } from '@jest/globals'
+import { ChartManager, ConfigManager, Helm, Kubectl, logging } from '../../../src/core/index.mjs'
+import { BaseCommand } from '../../../src/commands/base.mjs'
+import * as core from '../../../src/core/index.mjs'
+import { Kind } from '../../../src/core/kind.mjs'
 
-const testLogger = logging.NewLogger("debug")
+const testLogger = logging.NewLogger('debug')
 
 describe('BaseCommand', () => {
-    const kind = new Kind(testLogger)
-    const helm = new Helm(testLogger)
-    const kubectl = new Kubectl(testLogger)
-    const chartManager = new ChartManager(helm, testLogger)
-    const configManager = new ConfigManager(testLogger)
+  const kind = new Kind(testLogger)
+  const helm = new Helm(testLogger)
+  const kubectl = new Kubectl(testLogger)
+  const chartManager = new ChartManager(helm, testLogger)
+  const configManager = new ConfigManager(testLogger)
 
-    const baseCmd = new BaseCommand({
-        logger: testLogger,
-        kind: kind,
-        helm: helm,
-        kubectl: kubectl,
-        chartManager: chartManager,
-        configManager: configManager,
+  const baseCmd = new BaseCommand({
+    logger: testLogger,
+    kind,
+    helm,
+    kubectl,
+    chartManager,
+    configManager
+  })
+
+  describe('runShell', () => {
+    it('should fail during invalid program check', async () => {
+      await expect(baseCmd.run('INVALID_PROGRAM')).rejects.toThrowError()
+    })
+    it('should succeed during valid program check', async () => {
+      await expect(baseCmd.run('date')).resolves.not.toBeNull()
+    })
+  })
+
+  describe('checks', () => {
+    it('should succeed with checkKind', async () => {
+      await expect(baseCmd.checkKind()).resolves.toBe(true)
     })
 
-    describe('runShell', () => {
-        it('should fail during invalid program check', async() => {
-            await expect(baseCmd.run("INVALID_PROGRAM")).rejects.toThrowError()
-        })
-        it('should succeed during valid program check', async() => {
-            await expect(baseCmd.run("date")).resolves.not.toBeNull()
-        })
+    it('should succeed with checkHelm', async () => {
+      await expect(baseCmd.checkHelm()).resolves.toBe(true)
     })
 
-    describe('checks', () => {
-        it('should succeed with checkKind', async() => {
-            await expect(baseCmd.checkKind()).resolves.toBe(true)
-        })
-
-        it('should succeed with checkHelm', async() => {
-            await expect(baseCmd.checkHelm()).resolves.toBe(true)
-        })
-
-        it('should succeed with checkKubectl', async() => {
-            await expect(baseCmd.checkKubectl()).resolves.toBe(true)
-        })
+    it('should succeed with checkKubectl', async () => {
+      await expect(baseCmd.checkKubectl()).resolves.toBe(true)
     })
+  })
 
-    describe('checkDependencies', () => {
-        it('should fail during invalid dependency check', async() => {
-            await expect(baseCmd.checkDependencies([core.constants.KIND, "INVALID"])).resolves.toBe(false)
-        })
-        it('should succeed during valid dependencies check', async() => {
-            await expect(baseCmd.checkDependencies([core.constants.KIND, core.constants.HELM])).resolves.toBe(true)
-        })
+  describe('checkDependencies', () => {
+    it('should fail during invalid dependency check', async () => {
+      await expect(baseCmd.checkDependencies([core.constants.KIND, 'INVALID'])).resolves.toBe(false)
     })
+    it('should succeed during valid dependencies check', async () => {
+      await expect(baseCmd.checkDependencies([core.constants.KIND, core.constants.HELM])).resolves.toBe(true)
+    })
+  })
 })
--- fullstack-network-manager/test/unit/core/platform_installer.test.js
+++ fullstack-network-manager/test/unit/core/platform_installer.test.js
@@ -1,118 +1,118 @@
-import {describe, expect, it, test} from "@jest/globals";
-import * as core from "../../../src/core/index.mjs";
-import {PlatformInstaller} from "../../../src/core/index.mjs";
+import { describe, expect, it } from '@jest/globals'
+import * as core from '../../../src/core/index.mjs'
+import { PlatformInstaller } from '../../../src/core/index.mjs'
 import * as fs from 'fs'
-import * as path from "path";
-import * as os from "os";
+import * as path from 'path'
+import * as os from 'os'
 import {
-    IllegalArgumentError,
-    MissingArgumentError,
-} from "../../../src/core/errors.mjs";
+  IllegalArgumentError,
+  MissingArgumentError
+} from '../../../src/core/errors.mjs'
 describe('PackageInstaller', () => {
-    const testLogger = core.logging.NewLogger('debug')
-    const kubectl = new core.Kubectl(testLogger)
-    const installer = new PlatformInstaller(testLogger, kubectl)
+  const testLogger = core.logging.NewLogger('debug')
+  const kubectl = new core.Kubectl(testLogger)
+  const installer = new PlatformInstaller(testLogger, kubectl)
 
-    describe('validatePlatformReleaseDir', () => {
-        it('should fail for missing path', async () => {
-            expect.assertions(1)
-            await expect(installer.validatePlatformReleaseDir('')).rejects.toThrow(MissingArgumentError)
-        })
-
-        it('should fail for invalid path', async () => {
-            expect.assertions(1)
-            await expect(installer.validatePlatformReleaseDir('/INVALID')).rejects.toThrow(IllegalArgumentError)
-        })
-
-        it('should fail if directory does not have data/apps directory', async () => {
-            expect.assertions(1)
-
-            let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'));
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, {recursive: true})
-            await expect(installer.validatePlatformReleaseDir(tmpDir)).rejects.toThrow(IllegalArgumentError)
-            fs.rmdirSync(tmpDir, {recursive: true})
-        })
-
-        it('should fail if directory does not have data/libs directory', async () => {
-            expect.assertions(1)
-
-            let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'));
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, {recursive: true})
-            await expect(installer.validatePlatformReleaseDir(tmpDir)).rejects.toThrow(IllegalArgumentError)
-            fs.rmdirSync(tmpDir, {recursive: true})
-        })
-
-        it('should fail if directory does not have data/app directory is empty', async () => {
-            expect.assertions(1)
-
-            let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'));
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, {recursive: true})
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, {recursive: true})
-            fs.writeFileSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}/test.jar`, '')
-            await expect(installer.validatePlatformReleaseDir()).rejects.toThrow(MissingArgumentError)
-            fs.rmdirSync(tmpDir, {recursive: true})
-        })
-
-        it('should fail if directory does not have data/libs directory is empty', async () => {
-            expect.assertions(1)
-
-            let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'));
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, {recursive: true})
-            fs.writeFileSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}/app.jar`, '')
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, {recursive: true})
-            await expect(installer.validatePlatformReleaseDir()).rejects.toThrow(MissingArgumentError)
-            fs.rmdirSync(tmpDir, {recursive: true})
-        })
-
-        it('should succeed with non-empty data/apps and data/libs directory', async () => {
-            expect.assertions(1)
-
-            let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'));
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, {recursive: true})
-            fs.writeFileSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}/app.jar`, '')
-            fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, {recursive: true})
-            fs.writeFileSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}/lib-1.jar`, '')
-            await expect(installer.validatePlatformReleaseDir()).rejects.toThrow(MissingArgumentError)
-            fs.rmdirSync(tmpDir, {recursive: true})
-        })
+  describe('validatePlatformReleaseDir', () => {
+    it('should fail for missing path', async () => {
+      expect.assertions(1)
+      await expect(installer.validatePlatformReleaseDir('')).rejects.toThrow(MissingArgumentError)
     })
 
-    describe('extractPlatform', () => {
-        it('should fail for missing pod name', async () => {
-            expect.assertions(1)
-            await expect(installer.copyPlatform('', os.tmpdir())).rejects.toThrow(MissingArgumentError)
-        })
-        it('should fail for missing buildZipFile path', async () => {
-            expect.assertions(1)
-            await expect(installer.copyPlatform('network-node0-0', '' )).rejects.toThrow(MissingArgumentError)
-        })
+    it('should fail for invalid path', async () => {
+      expect.assertions(1)
+      await expect(installer.validatePlatformReleaseDir('/INVALID')).rejects.toThrow(IllegalArgumentError)
     })
 
-    describe('prepareConfigTxt', () => {
-        it('should fail for missing nodeIDs', async () => {
-            await expect(installer.prepareConfigTxt([],'./test', '0.42.0' )).rejects.toThrow(MissingArgumentError)
-        })
+    it('should fail if directory does not have data/apps directory', async () => {
+      expect.assertions(1)
 
-        it('should fail for missing destPath', async () => {
-            await expect(installer.prepareConfigTxt(['node0'],'', '0.42.0' )).rejects.toThrow(MissingArgumentError)
-        })
-
-        it('should fail for missing release tag', async () => {
-            await expect(installer.prepareConfigTxt(['node0'],`${os.tmpdir()}/config.txt`, '' )).rejects.toThrow(MissingArgumentError)
-        })
-
-        it('should fail for invalid destPath', async () => {
-            await expect(installer.prepareConfigTxt(['node0'],'/INVALID/config.txt', '0.42.0' )).rejects.toThrow(IllegalArgumentError)
-        })
+      const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'))
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, { recursive: true })
+      await expect(installer.validatePlatformReleaseDir(tmpDir)).rejects.toThrow(IllegalArgumentError)
+      fs.rmdirSync(tmpDir, { recursive: true })
     })
 
-    describe('copyGossipKeys', () => {
-        it('should fail for missing podName', async () => {
-            await expect(installer.copyGossipKeys('', os.tmpdir())).rejects.toThrow(MissingArgumentError)
-        })
+    it('should fail if directory does not have data/libs directory', async () => {
+      expect.assertions(1)
 
-        it('should fail for missing stagingDir path', async () => {
-            await expect(installer.copyGossipKeys('network-node0-0', '')).rejects.toThrow(MissingArgumentError)
-        })
+      const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'))
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, { recursive: true })
+      await expect(installer.validatePlatformReleaseDir(tmpDir)).rejects.toThrow(IllegalArgumentError)
+      fs.rmdirSync(tmpDir, { recursive: true })
     })
+
+    it('should fail if directory does not have data/app directory is empty', async () => {
+      expect.assertions(1)
+
+      const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'))
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, { recursive: true })
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, { recursive: true })
+      fs.writeFileSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}/test.jar`, '')
+      await expect(installer.validatePlatformReleaseDir()).rejects.toThrow(MissingArgumentError)
+      fs.rmdirSync(tmpDir, { recursive: true })
+    })
+
+    it('should fail if directory does not have data/libs directory is empty', async () => {
+      expect.assertions(1)
+
+      const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'))
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, { recursive: true })
+      fs.writeFileSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}/app.jar`, '')
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, { recursive: true })
+      await expect(installer.validatePlatformReleaseDir()).rejects.toThrow(MissingArgumentError)
+      fs.rmdirSync(tmpDir, { recursive: true })
+    })
+
+    it('should succeed with non-empty data/apps and data/libs directory', async () => {
+      expect.assertions(1)
+
+      const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'installer-'))
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}`, { recursive: true })
+      fs.writeFileSync(`${tmpDir}/${core.constants.DATA_APPS_DIR}/app.jar`, '')
+      fs.mkdirSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}`, { recursive: true })
+      fs.writeFileSync(`${tmpDir}/${core.constants.DATA_LIB_DIR}/lib-1.jar`, '')
+      await expect(installer.validatePlatformReleaseDir()).rejects.toThrow(MissingArgumentError)
+      fs.rmdirSync(tmpDir, { recursive: true })
+    })
+  })
+
+  describe('extractPlatform', () => {
+    it('should fail for missing pod name', async () => {
+      expect.assertions(1)
+      await expect(installer.copyPlatform('', os.tmpdir())).rejects.toThrow(MissingArgumentError)
+    })
+    it('should fail for missing buildZipFile path', async () => {
+      expect.assertions(1)
+      await expect(installer.copyPlatform('network-node0-0', '')).rejects.toThrow(MissingArgumentError)
+    })
+  })
+
+  describe('prepareConfigTxt', () => {
+    it('should fail for missing nodeIDs', async () => {
+      await expect(installer.prepareConfigTxt([], './test', '0.42.0')).rejects.toThrow(MissingArgumentError)
+    })
+
+    it('should fail for missing destPath', async () => {
+      await expect(installer.prepareConfigTxt(['node0'], '', '0.42.0')).rejects.toThrow(MissingArgumentError)
+    })
+
+    it('should fail for missing release tag', async () => {
+      await expect(installer.prepareConfigTxt(['node0'], `${os.tmpdir()}/config.txt`, '')).rejects.toThrow(MissingArgumentError)
+    })
+
+    it('should fail for invalid destPath', async () => {
+      await expect(installer.prepareConfigTxt(['node0'], '/INVALID/config.txt', '0.42.0')).rejects.toThrow(IllegalArgumentError)
+    })
+  })
+
+  describe('copyGossipKeys', () => {
+    it('should fail for missing podName', async () => {
+      await expect(installer.copyGossipKeys('', os.tmpdir())).rejects.toThrow(MissingArgumentError)
+    })
+
+    it('should fail for missing stagingDir path', async () => {
+      await expect(installer.copyGossipKeys('network-node0-0', '')).rejects.toThrow(MissingArgumentError)
+    })
+  })
 })
