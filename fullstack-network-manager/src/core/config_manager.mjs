2b927dff301c413eeef15baa042d7500eafc7033
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-11-16 21:37:12 +1100

    style: code format
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- fullstack-network-manager/src/core/config_manager.mjs
+++ fullstack-network-manager/src/core/config_manager.mjs
@@ -17,9 +17,9 @@ export class ConfigManager {
   }
 
   /**
-     * load package.json
-     * @returns {any}
-     */
+   * load package.json
+   * @returns {any}
+   */
   loadPackageJSON () {
     try {
       const raw = fs.readFileSync(`${CUR_FILE_DIR}/../../package.json`)
@@ -42,70 +42,69 @@ export class ConfigManager {
   }
 
   /**
-     * Load and store config
-     *
-     * It overwrites previous config values using opts and store in the config file if any value has been changed.
-     *
-     * @param opts object containing various config related fields (e.g. argv)
-     * @param reset if we should reset old values
-     * @returns {Promise<unknown>}
-     */
+   * Load and store config
+   *
+   * It overwrites previous config values using opts and store in the config file if any value has been changed.
+   *
+   * @param opts object containing various config related fields (e.g. argv)
+   * @param reset if we should reset old values
+   * @returns {Promise<unknown>}
+   */
   async setupConfig (opts, reset = false) {
     const self = this
 
-    return new Promise((resolve, reject) => {
-      try {
-        let config = {}
-        let writeConfig = false
-        const packageJSON = self.loadPackageJSON()
+    try {
+      let config = {}
+      let writeConfig = false
+      const packageJSON = self.loadPackageJSON()
 
-        // if config exist, then load it first
-        if (!reset && fs.existsSync(constants.FST_CONFIG_FILE)) {
-          const configJSON = fs.readFileSync(constants.FST_CONFIG_FILE)
-          config = JSON.parse(configJSON.toString())
-        }
-
-        if (!config.flags) {
-          config.flags = {}
-        }
-
-        // we always use packageJSON version as the version, so overwrite.
-        config.version = packageJSON.version
-
-        // extract flags from argv
-        if (opts) {
-          flags.allFlags.forEach(flag => {
-            if (opts && opts[flag.name]) {
-              let val = opts[flag.name]
-              if (val && flag.name === flags.chartDirectory.name) {
-                val = paths.resolve(val)
-              }
-
-              config.flags[flag.name] = val
-              writeConfig = true
-            }
-          })
-
-          // store last command that was run
-          if (opts._) {
-            config.lastCommand = opts._
-          }
-        }
-
-        // store CLI config
-        if (reset || writeConfig) {
-          config.updatedAt = new Date().toISOString()
-
-          let configJSON = JSON.stringify(config)
-          fs.writeFileSync(`${constants.FST_CONFIG_FILE}`, configJSON)
-          configJSON = fs.readFileSync(constants.FST_CONFIG_FILE)
-          config = JSON.parse(configJSON.toString())
-        }
-
-        resolve(config)
-      } catch (e) {
-        reject(new FullstackTestingError(`failed to load config: ${e.message}`, e))
+      // if config exist, then load it first
+      if (!reset && fs.existsSync(constants.FST_CONFIG_FILE)) {
+        const configJSON = fs.readFileSync(constants.FST_CONFIG_FILE)
+        config = JSON.parse(configJSON.toString())
       }
-    })
+
+      if (!config.flags) {
+        config.flags = {}
+      }
+
+      // we always use packageJSON version as the version, so overwrite.
+      config.version = packageJSON.version
+
+      // extract flags from argv
+      if (opts) {
+        flags.allFlags.forEach(flag => {
+          if (opts && opts[flag.name]) {
+            let val = opts[flag.name]
+            if (val && flag.name === flags.chartDirectory.name) {
+              console.log(val)
+              val = paths.resolve(val)
+            }
+
+            config.flags[flag.name] = val
+            writeConfig = true
+          }
+        })
+
+        // store last command that was run
+        if (opts._) {
+          config.lastCommand = opts._
+        }
+      }
+
+      // store CLI config
+      if (reset || writeConfig) {
+        config.updatedAt = new Date().toISOString()
+
+        let configJSON = JSON.stringify(config)
+        fs.writeFileSync(`${constants.FST_CONFIG_FILE}`, configJSON)
+        configJSON = fs.readFileSync(constants.FST_CONFIG_FILE)
+        config = JSON.parse(configJSON.toString())
+      }
+
+      return config
+    } catch (e) {
+      throw new FullstackTestingError(`failed to load config: ${e.message}`, e)
+    }
   }
 }
