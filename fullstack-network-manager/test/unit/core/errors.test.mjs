a8c3c9679d65e1221833f8606464f62dd69b9d3c
Quan Nguyen <quan@swirldslabs.com>
2023-11-17 10:56:55 -0600

    Fix Codacy issues.
    Signed-off-by: Quan Nguyen <quan@swirldslabs.com>

--- fullstack-network-manager/test/unit/core/errors.test.mjs
+++ fullstack-network-manager/test/unit/core/errors.test.mjs
@@ -1,62 +1,62 @@
-import {describe, expect, it} from '@jest/globals';
+import { describe, expect, it } from '@jest/globals'
 import {
   FullstackTestingError,
   ResourceNotFoundError,
   MissingArgumentError,
   IllegalArgumentError,
   DataValidationError
-} from '../../../src/core/errors.mjs';
+} from '../../../src/core/errors.mjs'
 
 describe('Errors', () => {
-  const message = 'errorMessage';
-  const cause = 'cause';
+  const message = 'errorMessage'
+  const cause = 'cause'
 
   it('should construct correct FullstackTestingError', () => {
-    const error = new FullstackTestingError(message, cause);
-    expect(error).toBeInstanceOf(Error);
-    expect(error.name).toBe('FullstackTestingError');
-    expect(error.message).toBe(message);
-    expect(error.cause).toBe(cause);
+    const error = new FullstackTestingError(message, cause)
+    expect(error).toBeInstanceOf(Error)
+    expect(error.name).toBe('FullstackTestingError')
+    expect(error.message).toBe(message)
+    expect(error.cause).toBe(cause)
     expect(error.meta).toStrictEqual({})
   })
 
   it('should construct correct ResourceNotFoundError', () => {
-    const resource = 'resource';
-    const error = new ResourceNotFoundError(message, resource);
-    expect(error).toBeInstanceOf(FullstackTestingError);
-    expect(error.name).toBe('ResourceNotFoundError');
-    expect(error.message).toBe(message);
+    const resource = 'resource'
+    const error = new ResourceNotFoundError(message, resource)
+    expect(error).toBeInstanceOf(FullstackTestingError)
+    expect(error.name).toBe('ResourceNotFoundError')
+    expect(error.message).toBe(message)
     expect(error.cause).toStrictEqual({})
-    expect(error.meta).toStrictEqual({ resource });
+    expect(error.meta).toStrictEqual({ resource })
   })
 
   it('should construct correct MissingArgumentError', () => {
-    const error = new MissingArgumentError(message);
-    expect(error).toBeInstanceOf(FullstackTestingError);
-    expect(error.name).toBe('MissingArgumentError');
-    expect(error.message).toBe(message);
+    const error = new MissingArgumentError(message)
+    expect(error).toBeInstanceOf(FullstackTestingError)
+    expect(error.name).toBe('MissingArgumentError')
+    expect(error.message).toBe(message)
     expect(error.cause).toStrictEqual({})
-    expect(error.meta).toStrictEqual({});
+    expect(error.meta).toStrictEqual({})
   })
 
   it('should construct correct IllegalArgumentError', () => {
-    const value = 'invalid argument';
-    const error = new IllegalArgumentError(message, value);
-    expect(error).toBeInstanceOf(FullstackTestingError);
-    expect(error.name).toBe('IllegalArgumentError');
-    expect(error.message).toBe(message);
+    const value = 'invalid argument'
+    const error = new IllegalArgumentError(message, value)
+    expect(error).toBeInstanceOf(FullstackTestingError)
+    expect(error.name).toBe('IllegalArgumentError')
+    expect(error.message).toBe(message)
     expect(error.cause).toStrictEqual({})
-    expect(error.meta).toStrictEqual({ value });
+    expect(error.meta).toStrictEqual({ value })
   })
 
   it('should construct correct DataValidationError', () => {
-    const expected = 'expected';
-    const found = 'found';
-    const error = new DataValidationError(message, expected, found);
-    expect(error).toBeInstanceOf(FullstackTestingError);
-    expect(error.name).toBe('DataValidationError');
-    expect(error.message).toBe(message);
+    const expected = 'expected'
+    const found = 'found'
+    const error = new DataValidationError(message, expected, found)
+    expect(error).toBeInstanceOf(FullstackTestingError)
+    expect(error.name).toBe('DataValidationError')
+    expect(error.message).toBe(message)
     expect(error.cause).toStrictEqual({})
-    expect(error.meta).toStrictEqual({ expected, found });
+    expect(error.meta).toStrictEqual({ expected, found })
   })
 })
