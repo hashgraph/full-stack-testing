288260528fdc0d9ee44fbda07484114e09b0aaca
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-11-30 13:46:24 +1100

    feat(cli): use prompts and progress status for chart commands
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- fullstack-network-manager/src/commands/base.mjs
+++ fullstack-network-manager/src/commands/base.mjs
@@ -1,15 +1,80 @@
 'use strict'
-import { MissingArgumentError } from '../core/errors.mjs'
-import { ShellRunner } from '../core/shell_runner.mjs'
-import * as flags from './flags.mjs'
+import {MissingArgumentError} from '../core/errors.mjs'
+import {ShellRunner} from '../core/shell_runner.mjs'
+import {constants} from '../core/index.mjs'
 
 export class BaseCommand extends ShellRunner {
-  async prepareChartPath (config, chartRepo, chartName) {
-    if (!config) throw new MissingArgumentError('config is required')
+  async checkDep(cmd) {
+    try {
+      this.logger.debug(cmd)
+      await this.run(cmd)
+    } catch (e) {
+      this.logger.showUserError(e)
+      return false
+    }
+
+    return true
+  }
+
+  /**
+   * Check if 'kind' CLI program is installed or not
+   * @returns {Promise<boolean>}
+   */
+  async checkKind() {
+    return this.checkDep(`${constants.KIND} --version`)
+  }
+
+  /**
+   * Check if 'helm' CLI program is installed or not
+   * @returns {Promise<boolean>}
+   */
+  async checkHelm() {
+    return this.checkDep(`${constants.HELM} version`)
+  }
+
+  /**
+   * Check if 'kubectl' CLI program is installed or not
+   * @returns {Promise<boolean>}
+   */
+  async checkKubectl() {
+    return this.checkDep(`${constants.KUBECTL} version --client`)
+  }
+
+  /**
+   * Check if all the required dependencies are installed or not
+   * @param deps is a list of dependencies
+   * @returns {Promise<boolean>}
+   */
+  async checkDependencies(deps = []) {
+    this.logger.debug('Checking for required dependencies: %s', deps)
+
+    for (let i = 0; i < deps.length; i++) {
+      const dep = deps[i]
+      this.logger.debug("Checking for dependency '%s'", dep)
+
+      let status = false
+      const check = this.checks.get(dep)
+      if (check) {
+        status = await check()
+      }
+
+      if (!status) {
+        this.logger.showUser(chalk.red(`FAIL: '${dep}' is not found`))
+        return false
+      }
+
+      this.logger.showUser(chalk.green(`OK: '${dep}' is found`))
+    }
+
+    this.logger.debug('All required dependencies are found: %s', deps)
+
+    return true
+  }
+
+  async prepareChartPath(chartDir, chartRepo, chartName) {
     if (!chartRepo) throw new MissingArgumentError('chart repo name is required')
     if (!chartName) throw new MissingArgumentError('chart name is required')
 
-    const chartDir = this.configManager.flagValue(config, flags.chartDirectory)
     if (chartDir) {
       const chartPath = `${chartDir}/${chartName}`
       await this.helm.dependency('update', chartPath)
@@ -19,7 +84,7 @@ export class BaseCommand extends ShellRunner {
     return `${chartRepo}/${chartName}`
   }
 
-  constructor (opts) {
+  constructor(opts) {
     if (!opts || !opts.logger) throw new Error('An instance of core/Logger is required')
     if (!opts || !opts.kind) throw new Error('An instance of core/Kind is required')
     if (!opts || !opts.helm) throw new Error('An instance of core/Helm is required')
