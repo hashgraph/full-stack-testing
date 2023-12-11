bb387e57b1de9817a944df482effebb189ee650a
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2023-12-11 11:35:38 +1100

    use latest generate.sh
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- dev/local-node/data/keys3/generate.sh
+++ dev/local-node/data/keys3/generate.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
-cd "`dirname "$0"`"
+#
+# Copyright 2016-2022 Hedera Hashgraph, LLC
+#
+# This software is the confidential and proprietary information of
+# Hedera Hashgraph, LLC. ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Hedera Hashgraph.
+#
+# HEDERA HASHGRAPH MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+# THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+# TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE, OR NON-INFRINGEMENT. HEDERA HASHGRAPH SHALL NOT BE LIABLE FOR
+# ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+# DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+#
+
+cd "$(dirname "$0")"
 
 # check if the names were supplied as arguments
-if [[ -z "$@" ]]; then
+if [[ -z "$*" ]]; then
   # if not, use a default set of names
   names=("alice" "bob" "carol" "dave")
 else
-  names=( "$@" )
+  names=("$@")
 fi
-
-
 
 #names=(`cat names.txt`)
 
@@ -30,66 +45,30 @@ mkdir unused 2>/dev/null
 mv *.pfx unused 2>/dev/null
 rmdir unused 2>/dev/null
 
-# When gen_steram_key is set to 1, an extra pair of key used for streaming will be generated
-# and added to keystore
-
-gen_stream_key=1
-
 for nm in "${names[@]}"; do
-   n="$(echo $nm | tr '[A-Z]' '[a-z]')"
-   keytool    -genkeypair -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=s-$n" -keyalg "rsa" -sigalg "SHA384withRSA" -keysize "3072" -validity "36500"
-   keytool    -genkeypair -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=a-$n" -keyalg "ec" -sigalg "SHA384withECDSA" -keysize "384" -validity "36500"
-   keytool    -genkeypair -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=e-$n" -keyalg "ec" -sigalg "SHA384withECDSA" -keysize "384" -validity "36500"
-   keytool    -certreq    -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -gencert    -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -validity "36500"  |
-      keytool -importcert -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
-   keytool    -certreq    -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -gencert    -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -validity "36500"  |
-      keytool -importcert -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
-   keytool    -exportcert -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -importcert -alias "s-$n" -keystore "public.pfx"     -storetype "pkcs12" -storepass "password"  -noprompt
-   keytool    -exportcert -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -importcert -alias "a-$n" -keystore "public.pfx"     -storetype "pkcs12" -storepass "password"  -noprompt
-   keytool    -exportcert -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -importcert -alias "e-$n" -keystore "public.pfx"     -storetype "pkcs12" -storepass "password"  -noprompt
+  n="$(echo $nm | tr '[A-Z]' '[a-z]')"
+  keytool -genkeypair -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=s-$n" -keyalg "rsa" -sigalg "SHA384withRSA" -keysize "3072" -validity "36524"
+  keytool -genkeypair -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=a-$n" -keyalg "ec" -sigalg "SHA384withECDSA" -groupname "secp384r1" -validity "36524"
+  keytool -genkeypair -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=e-$n" -keyalg "ec" -sigalg "SHA384withECDSA" -groupname "secp384r1" -validity "36524"
+  keytool -certreq -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" |
+    keytool -gencert -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -validity "36524" |
+    keytool -importcert -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
+  keytool -certreq -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" |
+    keytool -gencert -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -validity "36524" |
+    keytool -importcert -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
+  keytool -exportcert -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" |
+    keytool -importcert -alias "s-$n" -keystore "public.pfx" -storetype "pkcs12" -storepass "password" -noprompt
+  keytool -exportcert -alias "a-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" |
+    keytool -importcert -alias "a-$n" -keystore "public.pfx" -storetype "pkcs12" -storepass "password" -noprompt
+  keytool -exportcert -alias "e-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" |
+    keytool -importcert -alias "e-$n" -keystore "public.pfx" -storetype "pkcs12" -storepass "password" -noprompt
 
-
-   if [ $gen_stream_key == 1 ]
-   then
-     # generate stream key pair for client
-     keytool  -genkeypair -alias "k-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=k-$n" -keyalg "ec" -sigalg "SHA384withECDSA" -keysize "384" -validity "36500"
-
-     keytool    -certreq    -alias "k-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -gencert    -alias "s-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password" -validity "36500" |
-      keytool -importcert -alias "k-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
-
-    keytool    -exportcert -alias "k-$n" -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -importcert -alias "k-$n" -keystore "public.pfx"     -storetype "pkcs12" -storepass "password"  -noprompt
-   fi
-
-   echo "--------------------"
-   echo  "file: private-$n.pfx"
-   keytool -list                        -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
+  echo "--------------------"
+  echo "file: private-$n.pfx"
+  keytool -list -keystore "private-$n.pfx" -storetype "pkcs12" -storepass "password"
 done
-
-if [ $gen_stream_key == 1 ]
-then
-    # generate stream key pair for server
-     keytool  -genkeypair -alias "s-stream" -keystore "private-stream.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=s-stream" -keyalg "rsa" -sigalg "SHA384withRSA" -keysize "3072" -validity "36500"
-
-     keytool  -genkeypair -alias "k-stream" -keystore "private-stream.pfx" -storetype "pkcs12" -storepass "password" -dname "cn=k-stream" -keyalg "ec" -sigalg "SHA384withECDSA" -keysize "384" -validity "36500"
-
-    # export certificat
-    keytool    -certreq    -alias "k-stream" -keystore "private-stream.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -gencert    -alias "s-stream" -keystore "private-stream.pfx" -storetype "pkcs12" -storepass "password" -validity "36500" |
-      keytool -importcert -alias "k-stream" -keystore "private-stream.pfx" -storetype "pkcs12" -storepass "password"
-
-    # import to public.pfx
-    keytool    -exportcert -alias "k-stream" -keystore "private-stream.pfx" -storetype "pkcs12" -storepass "password"  |
-      keytool -importcert -alias "k-stream" -keystore "public.pfx"     -storetype "pkcs12" -storepass "password"  -noprompt
-fi
 
 echo "--------------------"
 echo "file: public.pfx"
-keytool  -list                          -keystore "public.pfx"    -storetype "pkcs12"  -storepass "password"
+keytool -list -keystore "public.pfx" -storetype "pkcs12" -storepass "password"
 ls
