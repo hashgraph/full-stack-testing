8dd0778b8f4f8669d5d3a1c7b9c876ae7c61ca5c
Jeromy Cannon <jeromy@swirldslabs.com>
2024-01-25 22:30:25 +0000

    updated to not run docker prune inside of script that could be run locally
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- fullstack-network-manager/test/e2e/setup-e2e.sh
+++ fullstack-network-manager/test/e2e/setup-e2e.sh
@@ -2,8 +2,6 @@
 FST_CLUSTER_NAME=fst-local
 FST_NAMESPACE=fst-local
 kind delete cluster -n "${FST_CLUSTER_NAME}" || true
-docker system prune -f
-docker image prune -f
 kind create cluster -n "${FST_CLUSTER_NAME}" || exit 1
 kubectl create ns "${FST_NAMESPACE}" || exit 1
 fsnetman init -d ../charts --namespace "${FST_NAMESPACE}" || exit 1 # cache args for subsequent commands
