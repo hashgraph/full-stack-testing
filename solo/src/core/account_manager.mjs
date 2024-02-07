c8df29cf3fd387d3b2fafc51e5e3ac0872889e37
Jeromy Cannon <jeromy@swirldslabs.com>
2024-02-07 09:18:31 +0000

    saving before rebase
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- solo/src/core/account_manager.mjs
+++ solo/src/core/account_manager.mjs
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the ""License"");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an ""AS IS"" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 export class AccountManager {
   constructor (logger, k8) {
     if (!logger) throw new Error('An instance of core/Logger is required')
@@ -13,9 +29,9 @@ export class AccountManager {
 
   async jeromyTesting (argv) {
     const { namespace, nodeIds } = argv
-    const labelSelector =
+    const labelSelector = ''
     const serviceList = await this.k8.kubeClient.listNamespacedService(
-        namespace, undefined, undefined, undefined, undefined, labelSelector)
+      namespace, undefined, undefined, undefined, undefined, labelSelector)
     // const clusterInfo = this.kubeClient.getCurrentCluster()
     const componentStatus = await this.k8.kubeClient.listComponentStatus()
     return {
