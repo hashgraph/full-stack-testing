fc11f0c0c00389ed732cb97ded95ae2763a142e1
Jeromy Cannon <jeromy@swirldslabs.com>
2024-02-21 14:11:51 +0000

    fix: update the address book on mirror node importer and restart it (#775)
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>
    Co-authored-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- solo/test/e2e/commands/02_account.test.mjs
+++ solo/test/e2e/commands/02_account.test.mjs
@@ -36,10 +36,7 @@ import { AccountManager } from '../../../src/core/account_manager.mjs'
 import { AccountCommand } from '../../../src/commands/account.mjs'
 import { flags } from '../../../src/commands/index.mjs'
 import { sleep } from '../../../src/core/helpers.mjs'
-import { FileContentsQuery, FileId } from '@hashgraph/sdk'
 import * as HashgraphProto from '@hashgraph/proto'
-import * as Base64 from 'js-base64'
-import fs from 'fs'
 
 describe('account commands should work correctly', () => {
   const defaultTimeout = 20000
@@ -212,14 +209,7 @@ describe('account commands should work correctly', () => {
     }
   }, defaultTimeout)
 
-  it('get NodeAddressBook', async () => {
-    // const originalBase64 = 'CvYGCgwxMC45Ni4xMzEuMzYaBTAuMC4zIswGMzA4MjAxYTIzMDBkMDYwOTJhODY0ODg2ZjcwZDAxMDEwMTA1MDAwMzgyMDE4ZjAwMzA4MjAxOGEwMjgyMDE4MTAwYTEyMDA2ZjI1MjcyNDJjZDdmNTNiOWM0ZmQ1ZGU4NjljNTYyZmY0ZWQ4YTRhYjNhNjI5NjNmM2Y4MTg4YmU3OGYyNThlYWY0MmI5YzNlM2U4ZjU4Njk3MTYwNDEzYmJmNzdhYWRmYWYwZDlmZmQ4Njk4NGIzYmM0ZmI1ZjAyZTExZThhY2I2ZWM2N2U0MjY0YWRkY2ExM2ZmZGU4MzY3NTc4YzI3OTJmYzZhZTMyNmU3YjM0ODU2ZDIzZjAyNTc0ZjVhMjc0ODE1NjAwMzJkNzM3NDlkYWY5ZDMzMmE3ODgzNTEwZTYwZWEyMjI4ODlhM2JmYzNiZWRmMzUwZjNhMzZjYWRhMzliOTQ1MzljNzRmYjgyNjU2NzhiZDVjMTQ4ZmJjOTE2OWFkY2FjZjY3MTliOTJlZThkNDJhOWQyZjg5MjNjYzRmZTNiNDk0YzQ2N2Y0ZTk4YTJhNDllMGEwZWY1YWE0MzNjYmVjNDM4YmFhMjU2YWU2MWEwNTkwY2U0ZTY3ZDBiM2ZlNWYyZGE5YWQ5MGMyNmUyMjlkMjhkNjc5NDU2OTc4ZDY1NzM1NzNlYmI0NjIzOWJhZDRhYmZhYjRmZmU2NGVhNGEzOTdjNmJjNWY3MTc1ZTFhYzM2NDg4ZjE3YzY3NzNiNWVkOTM0MTVlNTdjNDFjNmNkODg2NmRiZDEyMzYwMDYyMzM5Y2IxNTRkODVhZDFhZWNkMWQ3NjBhMDk0MWQ4MGUyOGZlMjQ2MDMzYTg2OGRiYmM4OTc1ZjM2MDQ0MzFmY2U5YWY5MWRjNDI1NDBhYjA0YjFiYmVjMjFmMzVmNzVmMzBhZjZmMDVjODk4NjQyOTM4NmUyYjQzOWJmZDJmZDU5MTJjODI2MDA4MDFlOWMwODU3ZjE2NWU4ODdmMjIzM2RjMmMwOThiYTEyNjdiMDU5ZGI4YTJkYjcxNGZlNTA2NjhkYmYwOTM2MDRhNWRmM2MyMzIxNDY3MjMyMTFjZTU4NTUyNjM4ZmFlOWZjMDY0YTdmMDliMDIwMzAxMDAwMTICGANCCgoECmCDJBC/hwNQAQ=='
-    // const decodedOriginalAddressBook = Base64.atob(originalBase64)
-    // const protoOriginalAddressBook = TestHelper.getFirstNodeAddress(Uint8Array.from(Array.from(decodedOriginalAddressBook).map(letter => letter.charCodeAt(0))))
-    // // const readContents = fs.readFileSync('/Users/user/source/full-stack-testing/solo/secret-addressbook.bin')
-    // // Base64.btoa(Base64.atob(originalBase64)) // matches
-    // const recodedAddressBook = Base64.encode(protoOriginalAddressBook.finish()) // matches
-    // // console.log(`protoOriginalAddressBook:\n${protoOriginalAddressBook}`)
+  it('update addressBook and restart importer', async () => {
     try {
       const ctx = {
         config: {}
@@ -227,40 +217,29 @@ describe('account commands should work correctly', () => {
       ctx.config.namespace = configManager.getFlag(flags.namespace)
       await accountCmd.loadTreasuryAccount(ctx)
       await accountCmd.loadNodeClient(ctx)
-      // Create the query
-      const fileQuery = new FileContentsQuery()
-        .setFileId(FileId.ADDRESS_BOOK)
 
-      // Sign with the operator private key and submit to a Hedera network
-      const contents = await fileQuery.execute(ctx.nodeClient)
-      const nodeAddressBook = TestHelper.getFirstNodeAddress(contents)
-      const base64NodeAddressBook = Base64.encode(nodeAddressBook.finish())
-      // const base64NodeAddressBook = Base64.btoa(contents.toString())
-      // const srcPath = path.join(argv[flags.cacheDir.name], 'addressbook.bin')
-      // fs.writeFileSync(srcPath, nodeAddressBook.finish())
-      // console.log(`srcPath: ${srcPath}`)
-      // console.log(`base64? ${base64NodeAddressBook}`)
+      // Retrieve the AddressBook as base64
+      const base64NodeAddressBook = await accountManager.prepareAddressBookBase64(ctx.nodeClient)
 
-      // const secret = await k8.getSecret(ctx.config.namespace, 'app.kubernetes.io/component=importer')
-      const result = await k8.kubeClient.listNamespacedSecret(
-        ctx.config.namespace, null, null, null, null, 'app.kubernetes.io/component=importer')
-      if (result.response.statusCode === 200 && result.body.items && result.body.items.length > 0) {
-        const secretObject = result.body.items[0]
+      // update kubernetes secrets
+      const secrets = await k8.getSecretsByLabel(['app.kubernetes.io/component=importer'])
+      for (const secretObject of secrets) {
         delete secretObject.metadata.creationTimestamp
         delete secretObject.metadata.managedFields
         delete secretObject.metadata.resourceVersion
         delete secretObject.metadata.uid
         secretObject.data['addressbook.bin'] = base64NodeAddressBook
-        // patch is broke, need to use delete/create: https://github.com/kubernetes-client/javascript/issues/893
-        // await k8.kubeClient.patchNamespacedSecret(secret.name, ctx.config.namespace, secret.data)
-        await k8.kubeClient.deleteNamespacedSecret(secretObject.metadata.name, ctx.config.namespace)
-        await k8.kubeClient.createNamespacedSecret(ctx.config.namespace, secretObject)
-        // console.log('done')
+
+        await k8.updateSecret(secretObject, ctx.config.namespace)
       }
 
-      // const srcPath = path.join(argv[flags.cacheDir.name], 'addressbook.bin')
-      // fs.writeFileSync(srcPath, nodeAddressBook.finish())
-      // await k8.copyTo('fullstack-deployment-importer-645457bd57-58n94', 'importer', srcPath, '/usr/etc/hedera/..2024_02_16_17_29_09.4048515600')
+      // restart the importer pod
+      const pods = await k8.getPodsByLabel(['app.kubernetes.io/component=importer'])
+      expect(pods.length).toBeGreaterThan(0)
+      for (const pod of pods) {
+        const resp = await k8.kubeClient.deleteNamespacedPod(pod.metadata.name, pod.metadata.namespace)
+        expect(resp.response.statusCode).toEqual(200)
+      }
     } catch (e) {
       testLogger.showUserError(e)
       expect(e).toBeNull()
@@ -269,22 +248,3 @@ describe('account commands should work correctly', () => {
     }
   }, defaultTimeout)
 })
-
-class TestHelper {
-  /**
-   * @internal
-   * @param {HashgraphProto.proto.INodeAddressBook} nodeAddressBook
-   * @returns {NodeAddressBook}
-   */
-  static getFirstNodeAddress (nodeAddressBook) {
-    const nodeAddress = HashgraphProto.proto.NodeAddressBook.decode(nodeAddressBook).nodeAddress[0]
-    nodeAddress.serviceEndpoint[0].ipAddressV4 = Uint8Array.from(nodeAddress.serviceEndpoint[0].ipAddressV4.toString().split('.'))
-    // nodeAddress.ipAddress = new Uint8Array(nodeAddress.ipAddress)
-    // nodeAddress.memo = new Uint8Array(nodeAddress.memo)
-    // nodeAddress.ipAddress = Uint8Array.from(Array.from('').map(letter => letter.charCodeAt(0)))
-    return HashgraphProto.proto.NodeAddressBook.encode(HashgraphProto.proto.NodeAddressBook.create({ nodeAddress: [nodeAddress] }))
-    // const nodeAddress = HashgraphProto.proto.NodeAddressBook.decode(nodeAddressBook).nodeAddress[0]
-    // return HashgraphProto.proto.NodeAddress.encode(nodeAddress)
-    // return HashgraphProto.proto.NodeAddressBook.encode(HashgraphProto.proto.NodeAddressBook.decode(nodeAddressBook))
-  }
-}
