8880375f3f17cbb5f2269cae3002df882c43062b
Matt Riben <matt.riben@swirldslabs.com>
2024-03-07 11:58:17 -0600

    saving
    Signed-off-by: Matt Riben <matt.riben@swirldslabs.com>

--- lib/src/main/java/com/swirldslabs/fullstacktest/api/v3/EventBus.java
+++ lib/src/main/java/com/swirldslabs/fullstacktest/api/v3/EventBus.java
@@ -9,35 +9,35 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class EventBus {
-    interface Message {}
+    public interface Message {}
 
-    static class Subscription implements AutoCloseable {
+    public static class Subscription implements AutoCloseable {
         public final BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
         final Predicate<Message> filter;
-        private final Consumer<Subscription> cancel;
-        Subscription(Predicate<Message> filter, Consumer<Subscription> cancel) {
+        private final Consumer<Subscription> unsubscribe;
+        Subscription(Predicate<Message> filter, Consumer<Subscription> unsubscribe) {
             this.filter = filter;
-            this.cancel = cancel;
+            this.unsubscribe = unsubscribe;
         }
         @Override
         public void close() throws Exception {
-            cancel.accept(this);
+            unsubscribe.accept(this);
         }
     }
 
     private final Set<Subscription> subscriptions = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-    Subscription subscribe(Predicate<Message> filter) {
+    public Subscription subscribe(Predicate<Message> filter) {
         Subscription subscription = new Subscription(filter, this::unsubscribe);
         subscriptions.add(subscription);
         return subscription;
     }
 
-    void unsubscribe(Subscription subscription) {
+    public void unsubscribe(Subscription subscription) {
         subscriptions.remove(subscription);
     }
 
-    void publish(Message message) throws InterruptedException {
+    public void publish(Message message) throws InterruptedException {
         for (Subscription sub : subscriptions) {
             if (sub.filter.test(message)) {
                 sub.queue.put(message);
@@ -45,7 +45,7 @@ public class EventBus {
         }
     }
 
-    int subscribers() {
+    public int subscribers() {
         return subscriptions.size();
     }
 }
