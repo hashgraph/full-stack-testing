8c46f3b11b62d683a030f1e6bf049cc7d88f8603
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-10-08 13:20:47 -0500

    rm helm task
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- fullstack-examples/build.gradle.kts
+++ fullstack-examples/build.gradle.kts
@@ -14,11 +14,6 @@
  * limitations under the License.
  */
 
-import com.hedera.fullstack.gradle.plugin.HelmDependencyUpdateTask
-import com.hedera.fullstack.gradle.plugin.HelmInstallChartTask
-import com.hedera.fullstack.gradle.plugin.HelmReleaseExistsTask
-import com.hedera.fullstack.gradle.plugin.HelmTestChartTask
-import com.hedera.fullstack.gradle.plugin.HelmUninstallChartTask
 import com.hedera.fullstack.gradle.plugin.kind.release.KindArtifactTask
 
 plugins {
@@ -33,55 +28,6 @@ dependencies {
     implementation(platform("com.hedera.fullstack:fullstack-bom"))
 }
 
-tasks.register<HelmInstallChartTask>("helmInstallFstChart") {
-    createNamespace.set(true)
-    namespace.set("fst-ns")
-    release.set("fst")
-    chart.set("../charts/fullstack-deployment")
-}
-
-tasks.register<HelmInstallChartTask>("helmInstallNginxChart") {
-    createNamespace.set(true)
-    namespace.set("nginx-ns")
-    release.set("nginx-release")
-    chart.set("oci://ghcr.io/nginxinc/charts/nginx-ingress")
-}
-
-tasks.register<HelmUninstallChartTask>("helmUninstallNginxChart") {
-    namespace.set("nginx-ns")
-    release.set("nginx-release")
-}
-
-tasks.register<HelmReleaseExistsTask>("helmNginxExists") {
-    allNamespaces.set(true)
-    namespace.set("nginx-ns")
-    release.set("nginx-release")
-}
-
-tasks.register<HelmDependencyUpdateTask>("helmDependencyUpdate") {
-    chartName.set("../charts/fullstack-deployment")
-}
-
-tasks.register<HelmTestChartTask>("helmTestNginxChart") {
-    namespace.set("nginx-ns")
-    release.set("nginx-release")
-}
-
-// This task will succeed because it only uninstalls if the release exists
-tasks.register<HelmUninstallChartTask>("helmUninstallNotAChart") {
-    release.set("not-a-release")
-    ifExists.set(true)
-}
-
-val kindVersion = "0.20.0"
-
-tasks.register<KindArtifactTask>("kindArtifact") { version.set(kindVersion) }
 
 tasks.check {
-    dependsOn("helmInstallNginxChart")
-    dependsOn("helmNginxExists")
-    dependsOn("helmTestNginxChart")
-    dependsOn("helmUninstallNginxChart")
-    dependsOn("helmDependencyUpdate")
-    dependsOn("helmUninstallNotAChart")
 }
--- fullstack-gradle-plugin/src/main/kotlin/com/hedera/fullstack/gradle/plugin/kind/release/OperatingSystem.kt
+++
@@ -1,7 +0,0 @@
-package com.hedera.fullstack.gradle.plugin.kind.release
-
-enum class OperatingSystem(val descriptor: String, val fileExtension: String) {
-    DARWIN("darwin",  ""),
-    LINUX("linux", ""),
-    WINDOWS("windows", ".exe");
-}
