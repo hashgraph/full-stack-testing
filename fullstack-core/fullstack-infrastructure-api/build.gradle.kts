d4629dcfe9bafa9a9b5decb51ba02db82539d6b0
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-10-08 15:39:56 -0500

    save

--- fullstack-core/fullstack-infrastructure-api/build.gradle.kts
+++ fullstack-core/fullstack-infrastructure-api/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,4 @@ plugins {
     //    id("com.hedera.fullstack.maven-publish")
 }
 
-dependencies {
-    api(platform(project(":fullstack-bom")))
-}
+dependencies { api(platform(project(":fullstack-bom"))) }
