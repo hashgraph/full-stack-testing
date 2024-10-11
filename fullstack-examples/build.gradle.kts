e31bec9d5b377a9ffbe9148a3165e109468799a7
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-10-11 12:33:23 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- fullstack-examples/build.gradle.kts
+++ fullstack-examples/build.gradle.kts
@@ -45,9 +45,7 @@ tasks.register<HelmUninstallChartTask>("helmUninstallNginxChart") {
     release.set("nginx-release")
 }
 
-tasks.register<HelmDependencyUpdateTask>("helmDependencyUpdate") {
-    chartName.set("nginx-release")
-}
+tasks.register<HelmDependencyUpdateTask>("helmDependencyUpdate") { chartName.set("nginx-release") }
 
 tasks.register<HelmReleaseExistsTask>("helmNginxExists") {
     allNamespaces.set(true)
