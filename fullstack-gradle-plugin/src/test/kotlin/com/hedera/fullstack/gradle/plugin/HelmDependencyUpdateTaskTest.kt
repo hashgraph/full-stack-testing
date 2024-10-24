ed28fcd501abd774c62c7f1644d7e2d7103ff7fb
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-10-24 12:00:22 -0500

    revert
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- fullstack-gradle-plugin/src/test/kotlin/com/hedera/fullstack/gradle/plugin/HelmDependencyUpdateTaskTest.kt
+++ fullstack-gradle-plugin/src/test/kotlin/com/hedera/fullstack/gradle/plugin/HelmDependencyUpdateTaskTest.kt
@@ -40,7 +40,19 @@ class HelmDependencyUpdateTaskTest {
         }
     }
 
+    @Test
     @DisplayName("Test Helm Dependency Update Task")
+    internal fun testDependencyUpdate() {
+//        val helmDependencyUpdateTask = project.tasks.create(
+//            "helmDependencyUpdateTask",
+//            HelmDependencyUpdateTask::class
+//        ).apply {
+//            chartName.set(CHART)
+//            workingDirectory.set(File(".").toPath().toString())
+//        }
+//        assertThat(helmDependencyUpdateTask.chartName.get()).isEqualTo(CHART)
+//        helmDependencyUpdateTask.dependencyUpdate()
+    }
 
     @Test
     @DisplayName("Test Helm Dependency Update Task with no chart name")
